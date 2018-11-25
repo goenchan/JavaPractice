bb42eb8f8adbe8a57091341ac07d1957cb3fb8b0
Chan Lee <chanethanlee@gmail.com>
2018-11-25 00:03:21 -0800

    add equal number / is cat playing method.

--- BasicExerciseTwo/src/BasicExerciseTwo.java
+++ BasicExerciseTwo/src/BasicExerciseTwo.java
@@ -1,6 +1,6 @@
 public class BasicExerciseTwo {
     public static void main(String[] args) {
-        printYearsAndDays(641232);
+//        printYearsAndDays(641232);
     }
 
     public static double AreaCaculator (double radius) {
@@ -24,5 +24,26 @@ public class BasicExerciseTwo {
             int ZZ = (int) (minutes%525600)/ (60*24);
             System.out.println(XX+" min = "+YY+" y and "+ZZ+" d");
         }
+    }
+
+    public static void printEqual (int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+        } else if (a==b && b==c) {
+            System.out.println("All numbers are equal");
+        } else if (a!=b && b!=c && a!=c) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+
+    public static boolean isCatPlaying (boolean summer, int temperature) {
+        if (summer && temperature >= 25 && temperature <= 45) {
+            return true;
+        } else if (!summer && temperature >=25 && temperature <=35) {
+            return true;
+        }
+        return false;
     }
 }
